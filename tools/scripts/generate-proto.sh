ce67ee6b644c14be2548b25d492674861ea07d4d
--global <-g>
2024-08-31 09:16:30 +0200

    chore: Add proto library and generated files

---
+++ tools/scripts/generate-proto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROTO_DIR=infra/proto/src/proto
+OUT_DIR=infra/proto/src/generated
+
+# Generate JavaScript and TypeScript files from proto files
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
+./node_modules/.bin/grpc_tools_node_protoc \
+    --plugin="protoc-gen-ts=./node_modules/.bin/protoc-gen-ts_proto" \
+    --js_out=import_style=commonjs,binary:$OUT_DIR \
+    --ts_out=$OUT_DIR \
+    $PROTO_DIR/*.proto
+
+mv $OUT_DIR/$PROTO_DIR/* $OUT_DIR
+rm -rf $OUT_DIR/infra
